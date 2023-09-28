94f3654e121edbade838af99d9cacf3010c2b352
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-09-28 10:25:29 -0500

    feat(cdk): add s3 bucket configuration

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_s3 as s3 } from "aws-cdk-lib";
+import { config } from "dotenv";
 
-export class CdkStack extends cdk.Stack {
+config();
+export class LariosCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucketName = process.env.BUCKET_NAME || 'larios-bucket';
+    new s3.Bucket(this, bucketName, {
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
   }
 }
